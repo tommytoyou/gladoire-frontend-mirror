68a6872b2782de4c85fa289ef0c4e0522586fa8b
tommytoyoutoo <tommytoyou@gmail.com>
2021-03-11 23:43:11 -0600

    adding content testing css

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>This is about us...</p>
+            <h2>Meditations</h2>
+            <p>Entering the Jhanas...</p>
             <p><b>Samadhi</b>, (Sanskrit: “total self-collectedness”) in Indian philosophy
              and religion, and particularly in Hinduism and Buddhism, the highest state 
              of mental concentration that people can achieve while still bound to the body 
@@ -20,6 +20,13 @@ const About = () => {
              concentration, which is thought to lead to a one-pointedness of mind that allows the meditator to gain
               insight into the saving truth that all reality is impermanent, permeated by suffering, and devoid of self.
                This insight, from the Buddhist perspective, allows the meditator to progress toward the attainment of nirvana itself.
+            </p>
+            <br />
+            <p><b>Mindfulness</b>, Being mindful means being aware of our thoughts, emotions, and how we're feeling 
+            both physically and mentally. Mindfulness is a form of meditation with an important aspect to 
+            it—acceptance. It means being aware of our thoughts and feelings without judgment. Things are
+             what they are. We can then see cause and affect and choose to be outside of that cycle. A lttle at first,
+              then much more so. This has a profound influence on the manner in which we perceive our world and choose to live.
             </p>
             <br />
         </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,8 +3,20 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            <h1 class="text-center">May All Sentient Beings Realize Happiness</h1>
+            <h2 class="text-center">May All Sentient Beings Realize Happiness</h2>
+            <br />
+            <p>
+            Four stages, called (in Sanskrit) <b>dhyanas</b> or (in Pali) <b>jhanas</b>, are 
+            distinguished in the shift of attention from the outward sensory world:
+            <ul>
+                <li>First we experience a detachment from the external world and a consciousness of joy and ease, </li> 
+                <li>Then a concentration, with suppression of reasoning and investigation, </li>
+                <li>Followed by the passing away of joy, with the sense of ease remaining, and </li>
+                <li>Culminating in the passing away of ease also, bringing about a state of pure self-possession and equanimity.</li>
+            </ul> 
+            </p>
         </div>
+        
     )
 }
 
